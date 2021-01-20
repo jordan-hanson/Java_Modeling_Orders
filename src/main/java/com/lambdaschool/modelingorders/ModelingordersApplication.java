f02eb83c37f802a0909c3663a19a209469f5dee9
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-01-20 07:44:35 -0700

    initial commit

---
+++ src/main/java/com/lambdaschool/modelingorders/ModelingordersApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.modelingorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelingordersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelingordersApplication.class, args);
+    }
+
+}
